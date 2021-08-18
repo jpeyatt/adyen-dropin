75e530ec156d19c604af5ee88f5c57f8655f80da
Justin Peyatt <j.peyatt@criteo.com>
2021-08-17 20:45:29 -0400

    Cleanup Checkout comments.

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -13,15 +13,11 @@ const Checkout = ({ onPaymentResponse }) => {
     const history = useHistory([]);
 
     const onSubmit = (state, dropin) => {
-        // Global configuration for onSubmit
-        // Your function calling your server to make the `/payments` request
         makePayment(state.data)
             .then(response => {
                 if (response.action) {
-                    // Drop-in handles the action object from the /payments response
                     dropin.handleAction(response.action);
                 } else {
-                    // Your function to show the final result to the shopper
                     handlePaymentResponse(response);
                 }
             })
@@ -58,10 +54,8 @@ const Checkout = ({ onPaymentResponse }) => {
         makeDetailsCall(state.data)
             .then(response => {
                 if (response.action) {
-                    // Drop-in handles the action object from the /payments response
                     dropin.handleAction(response.action);
                 } else {
-                    // Your function to show the final result to the shopper
                     handlePaymentResponse(response, dropin);
                 }
             })
@@ -76,7 +70,6 @@ const Checkout = ({ onPaymentResponse }) => {
                 const checkout = new AdyenCheckout(res);
                 checkout
                     .create('dropin', {
-                        // Starting from version 4.0.0, Drop-in configuration only accepts props related to itself and cannot contain generic configuration like the onSubmit event.
                         openFirstPaymentMethod: true
                     })
                     .mount(dropinRef.current);
