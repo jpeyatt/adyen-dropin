b0bcac786f5e0c5439817c0180b4189a9331576b
Justin Peyatt <j.peyatt@criteo.com>
2021-08-17 22:30:02 -0400

    Update server's react static config.

--- server/server.js
+++ server/server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use(express.static('/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 }
 
